fdc6f2ec5efb34274327b1c070a1a25f7b9c82bb
chance <cchen@indocresearch.org>
2022-10-09 21:36:28 -0400

    Update app.py

--- 002-JenkinsCICD/app.py
+++ 002-JenkinsCICD/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Chance!"
+    return "Hello World Chance 1!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
