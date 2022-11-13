f751cc6ea73631519d1baa1a48779931e7c3b0e8
zhiren <zzhan@indocresearch.org>
2022-11-13 18:28:46 -0500

    revert some local changes

--- 002-JenkinsCICD/app.py
+++ 002-JenkinsCICD/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!22222222222"
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
