45f4ab9e8a58e3efdda839fbaf7dfac6b7b95905
VincentFW <okfengwei@gmail.com>
2023-01-10 03:56:27 +0000

    change app.py output

--- 002-JenkinsCICD/app.py
+++ 002-JenkinsCICD/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! Hello Devops!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
