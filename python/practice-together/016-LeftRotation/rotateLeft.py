13bfa6cbbd8a0487b74d5bc64ad3f1695bb01a4a
Chance Chen <chance.chen21@gmail.com>
2024-05-30 09:47:00 -0400

    Add Python Practice lab 16

---
+++ python/practice-together/016-LeftRotation/rotateLeft.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    rotated_arr = arr[d:] + arr[:d]
+    return rotated_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
