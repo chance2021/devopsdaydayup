8f9adc8c5bbf01b9de2e521532822e441e5cc9ca
Chance Chen <chance.chen21@gmail.com>
2024-06-19 09:40:09 -0400

    Add Python practice lab for 017

---
+++ python/practice-together/017-PrintTheElementsOfALinkedList/printLinkedList.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+# https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    current = head
+    while current is not None:
+        print(current.data)
+        current = current.next
+
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
