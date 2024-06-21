5c24b7172ed918bb4c06115e02206d1190cec0ae
Chance Chen <chance.chen21@gmail.com>
2024-06-21 15:50:24 -0400

    Python Practice Lab 18

---
+++ python/practice-together/018-InsertANodeAtTheTailOfALinkedList/insertNodeAtTail.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+
+# https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    new_node = SinglyLinkedListNode(data)
+    if head is None:
+        head = new_node
+    else:
+        current = head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
