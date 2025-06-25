ca08c13115ab6c8495d972e604a18c91163b0a19
Chance Chen <chance.chen21@gmail.com>
2025-06-25 06:30:18 -0400

    Fix: Rename azurerm_app_service_plan to azurerm_service_plan and update references

--- 024-GithubActionTerraformAzureFunctionStorageCICD/terraform/modules/function_storage/main.tf
+++ 024-GithubActionTerraformAzureFunctionStorageCICD/terraform/modules/function_storage/main.tf
@@ -18,24 +18,20 @@ resource "azurerm_storage_account" "storage" {
   min_tls_version          = "TLS1_2"
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   depends_on = [time_sleep.wait_for_rg]
   name                = "lab24-${var.environment}-${var.name}-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "FunctionApp"
-  reserved            = true
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
 resource "azurerm_function_app" "function" {
   name                       = "lab24-${var.environment}-${var.name}-func"
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  app_service_plan_id        = azurerm_service_plan.plan.id
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   version                    = "~4"
